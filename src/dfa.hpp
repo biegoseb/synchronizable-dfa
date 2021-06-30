61b634d53a0f8b54a3069c6b4240a90e44379d20
Diego <diego@archlinux.localdomain>
2021-06-30 10:15:54 -0500

    power set

--- src/dfa.hpp
+++ src/dfa.hpp
@@ -4,20 +4,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class DFA 
-{
+class DFA {
     string input;
     int number_states;
     int initial_state;
     int number_final_states;
-    unordered_set<int> non_final_states;
-    unordered_set<int> final_states;
-    map<pair<int, int>, int> transitions;
+    vector<string> states;
+    unordered_set<string> non_final_states;
+    unordered_set<string> final_states;
+    map<pair<string, int>, string> transitions;
 
     void build_dfa();
-    void add_final_state(int fs);
-    void add_transition(int p, int a, int q);
-    DFA* getB(DFA origin);
+    void add_final_state(const string& fs);
+    void add_transition(const string& p, int a, const string& q);
+    //DFA* getB(DFA origin);
+    DFA getB();
     
 public:
     DFA(const string& input);
@@ -25,54 +26,52 @@ public:
     void print_sets();
     unordered_set<string> get_nfs();
     unordered_set<string> get_fs();
-    map<pair<string, string>, string> get_transitions();
-    //void validate_string(const string& str);
+    map<pair<string, int>, string> get_transitions();
+    vector<string> power_set();
 };
 
-DFA::DFA(const string& input) : input(input) 
-{
+DFA::DFA(const string& input) : input(input) {
     build_dfa();
 }
 
-void DFA::build_dfa()
-{   
+void DFA::build_dfa() {   
     fstream file("../input/" + input + ".txt", ios::in);
 
     /* first line */
     file >> number_states >> initial_state >> number_final_states;
     int fs;
-    for (size_t i = 0; i < number_final_states; ++i)
-    {   
+    for (size_t i = 0; i < number_final_states; ++i) {   
         file >> fs;
-        add_final_state(fs);
+        add_final_state(to_string(fs));
     }
-    for (size_t i = 0; i < number_states; ++i)
-    {
-        if (final_states.find(i) == final_states.end())
-            non_final_states.insert(i);
+    for (size_t i = 0; i < number_states; ++i) {
+        string state = to_string(i);
+        if (final_states.find(state) == final_states.end())
+            non_final_states.insert(state);
     }
     
     /* transitions */
     int p, a, q;
-    for (size_t i = 0; i < 2 * number_states; ++i)
-    {
+    for (size_t i = 0; i < 2 * number_states; ++i) {
         file >> p >> a >> q;
-        add_transition(p, a, q);
+        add_transition(to_string(p), a, to_string(q));
+    }
+
+    for (size_t i = 0; i < number_states; ++i) {
+        string state = to_string(i);
+        states.push_back(state);
     }
 }
 
-void DFA::add_final_state(int fs)
-{
+void DFA::add_final_state(const string& fs) {
     final_states.insert(fs);
 }
 
-void DFA::add_transition(int p, int a, int q)
-{
+void DFA::add_transition(const string& p, int a, const string& q) {
     transitions[make_pair(p, a)] = q;
 }
 
-void DFA::print_dfa()
-{
+void DFA::print_dfa() {
     cout << number_states << " " << initial_state << " " << number_final_states << " ";
     for (const auto& fs : final_states)
         cout << fs << " ";
@@ -83,8 +82,7 @@ void DFA::print_dfa()
     
 }
 
-void DFA::print_sets()
-{
+void DFA::print_sets() {
     cout << "Non final states: ";
     for (auto it = non_final_states.cbegin(); it != non_final_states.cend(); ++it)
         cout << *it << " ";
@@ -93,31 +91,54 @@ void DFA::print_sets()
     for (auto it = final_states.cbegin(); it != final_states.cend(); ++it)
         cout << *it << " ";
     cout << endl;
+    cout << "Power Set: ";
+    for (const auto s : power_set()) {
+        cout << s << " ";
+    }
+    cout << endl;
 }
 
-
-
-unordered_set<int> DFA::get_nfs()
-{
+unordered_set<string> DFA::get_nfs() {
     return non_final_states;
 }
-unordered_set<int> DFA::get_fs()
-{
+
+unordered_set<string> DFA::get_fs() {
     return final_states;
 }
 
-map<pair<int, int>, int> DFA::get_transitions()
-{
+map<pair<string, int>, string> DFA::get_transitions() {
     return transitions;
 }
 
-DFA* DFA::getB(DFA origin) {
+vector<string> DFA::power_set() {
+    // O(2^n) algorithm
+    vector<string> pset;
+    unsigned int size = pow(2, number_states);
+    int counter, i;
+
+    for (counter = 0; counter < size; counter++) {
+        string state;
+        for (i = 0; i < number_states; i++) {           
+            if (counter & (1 << i))
+                state += states[i];           
+        }
+        pset.push_back(state);
+    }
+    return pset;
+}
+
+DFA DFA::getB() {
+ //
+}
+
+
+/*DFA* DFA::getB(DFA origin) {
     DFA* DFA_B = this;
 
     for (int j = 0; j < ; ++j) {
 
     }
-    /*double DFA_Bsize = 0;
+    double DFA_Bsize = 0;
     DFA_Bsize = 2 * (pow(2, (origin.number_states)) - 1);
     DFA_B->number_states = (DFA_Bsize/2)-1;
     unordered_set<int> Bstates = origin.non_final_states;
@@ -132,9 +153,9 @@ DFA* DFA::getB(DFA origin) {
 
             }
         }
-    }*/
+    }
     return DFA_B;
-}
+}*/
 
 
 #endif // DFA_HPP
